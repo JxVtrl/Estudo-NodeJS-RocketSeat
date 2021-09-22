582151f27cf5cc9a71eb88dd7c17b4cdc52f0acb
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-09-22 15:25:13 -0300

    :tada: Server Run

---
+++ Aula3/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express() // guardando o express em uma constante
+
+app.set('view engine', 'ejs') // dizendo ao express como irá renderizar as páginas
+
+app.get("/", (req, res) => { // rota raiz
+    res.render('index') // response.render('nome_da_pagina')
+})
+
+// app.get("/about", (req, res) => { // rota about
+//     res.render('about')
+// })
+
+app.listen(8080) // iniciando o servidor na porta 8080 (localhost:8080)
+console.log('rodando...')
