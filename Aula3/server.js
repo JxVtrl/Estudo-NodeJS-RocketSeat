69fb50ed39fecd9613e74f65548b11b4513c0e20
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-09-22 17:24:45 -0300

    Update server.js

--- Aula3/server.js
+++ Aula3/server.js
@@ -7,9 +7,9 @@ app.get("/", (req, res) => { // rota raiz
     res.render('index') // response.render('nome_da_pagina')
 })
 
-// app.get("/about", (req, res) => { // rota about
-//     res.render('about')
-// })
+app.get("/about", (req, res) => { // rota about
+    res.render('about')
+})
 
 app.listen(8080) // iniciando o servidor na porta 8080 (localhost:8080)
 console.log('rodando...')
