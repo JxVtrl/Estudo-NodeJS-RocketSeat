1fabde6040eb8b608c8782b305a11a997ec694d3
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-09-23 15:19:06 -0300

    :sparkles: new features

--- Aula3/server.js
+++ Aula3/server.js
@@ -4,11 +4,21 @@ const app = express() // guardando o express em uma constante
 app.set('view engine', 'ejs') // dizendo ao express como irá renderizar as páginas
 
 app.get("/", (req, res) => { // rota raiz
-    res.render('index') // response.render('nome_da_pagina')
+    const items = [
+        { title: 'D', message: 'esenvolver aplicações/serviços de forma fácil' },
+        { title: 'E', message: 'JS usa JS para renderizar HTML' },
+        { title: 'M', message: 'uito fácil de user' },
+        { title: 'A', message: 'prendizado legal' },
+        { title: 'I', message: 'nstall EJS' },
+        { title: 'S', message: 'intaxe simples' },
+    ]
+    res.render('pages/index', {
+        qualitys: items,
+    })
 })
 
 app.get("/about", (req, res) => { // rota about
-    res.render('about')
+    res.render('pages/about')
 })
 
 app.listen(8080) // iniciando o servidor na porta 8080 (localhost:8080)
